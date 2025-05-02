2a1da8ab7b90b365d78f8e0f574144b31bb68a9f
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2025-05-02 22:35:51 +0600

    Что-то в карточке случилось

--- src/assets/components/layout/AccountCard/AccountCard.jsx
+++ src/assets/components/layout/AccountCard/AccountCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './AccountCard.module.css';
 
-const AccountCard = ({ id, name, balance, onClick, isSelected }) => {
+const AccountCard = ({ accountId, name, balance, onClick, isSelected }) => {
   return (
     <div 
       className={`${styles.accountCard} ${isSelected ? styles.selected : ''}`}
       onClick={onClick}
     >
-      <h3>{id}</h3>
-      <h3>{name}</h3>
-      <p className={styles.balance}>{balance} ₽</p>
+      <div className={styles.accountHeader}>
+        <h3 className={styles.accountName}>{name}</h3>
+        <div className={styles.accountId}>{accountId}</div>
+      </div>
+      <div className={styles.accountBalance}>{balance} ₽</div>
     </div>
   );
 };
